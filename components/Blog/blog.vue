50219d2d905b042ab8d629e40a33467d052223ab
nisshii0313 <yuta19990313@gmail.com>
2021-01-06 19:03:19 +0900

    fix

---
+++ components/Blog/blog.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nuxt-content :document="content" />
+  </div>
+</template>
+
+<style>
+.blog-main {
+  padding-right: 8px;
+  padding-left: 8px;
+  line-height: 150%;
+}
+.blog-main img {
+  width: 100%;
+}
+.blog-main a:link { color: #0000ff; }
+.blog-main a:visited { color: #000080; }
+.blog-main a:hover { color: #ff0000; }
+.blog-main a:active { color: #ff8000; }
+</style>
+
+<script>
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Blog extends Vue {
+    @Prop({ type: Object, required: true })
+    content;
+}
+</script>
